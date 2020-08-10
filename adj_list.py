721848a6bb5976d9562c230f1d4027fb7622caf0
Eric Richards <esholundr@yahoo.com>
2020-08-10 10:42:59 -0600

    code for adj_list.py, pseudocode for bft

--- adj_list.py
+++ adj_list.py
@@ -1,26 +1,43 @@
 from queue import Queue
 from stack import Stack
 # lets do a graph adjacency list
+
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        pass
+        self.vertices = {}
 
     def add_vertex(self, vertex_id):
-        pass
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
-        pass
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("Vertex does not exist!")
 
     def get_neighbors(self, vertex_id):
-        pass
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex_id):
+        # create an empty queue and enqueue a starting vertex
+        # create a set to store the visited vertices
+
+        # while the queue is not empty:
+        # dequeue the first vertex
+
+        # if vertex has not been visited
+        # mark the vertex as visited
+        # print it for debug
+
+        # add all of its neighbors to the back of the queue
         pass
 
     def dft(self, starting_vertex_id):
         pass
-    
+
     def bfs(self, starting_vertex_id, target_vertex_id):
         pass
 
